e490f0d48a7da1bddeafabe4a00b3316931094b7
nassmohammed <nasser.mohammed@mail.utoronto.ca>
2024-10-22 12:24:32 -0400

    created

---
+++ Boris_fast copy.py
@@ -0,0 +1,125 @@
+import numpy as np
+import h5py
+from tqdm import tqdm
+from multiprocessing import Pool, set_start_method
+from numba import jit
+
+# Particle class definition
+class Particle:
+    def __init__(self, mass, charge):
+        self.mass = mass
+        self.charge = charge
+        self.r = np.zeros(3, dtype='float64')
+        self.u = np.zeros(3, dtype='float64')
+        self.gamma = 1.0  # Set initial gamma
+
+    def initPos(self, x, y, z):
+        self.r[:] = (x, y, z)
+
+    def initSpeed(self, ux, uy, uz):
+        self.u[:] = (ux, uy, uz)
+
+# Electric field function
+@jit(nopython=True)
+def E(r, E0=0.1):
+    return np.array([E0 * np.cos(np.pi * r[1])/ 2, E0 * np.cos(np.pi * r[2])/ 2, 0.0])
+
+# Magnetic field function
+@jit(nopython=True)
+def B(r, B0=1):
+    return np.array([B0 * np.sin(np.pi * r[2])/ 2, B0 * np.sin(np.pi * r[1])/ 2, B0])
+
+# Push function for particle simulation
+@jit(nopython=True)
+def push(r, u, gamma, charge, mass, dt):
+    rplus = r + u * dt / (2 * gamma)
+    u += charge * E(rplus) * dt / (2 * mass)
+    gamma_minus = np.sqrt(1 + np.sum(u ** 2))
+
+    # Calculate magnetic field effect
+    B_effect = charge * dt * B(rplus) / (2 * mass * gamma_minus)
+    t = B_effect
+    u1 = u + np.cross(u, t)
+    u += np.cross(u1, t)
+
+    u += charge * E(rplus) * dt / (2 * mass)
+    gamma = np.sqrt(1 + np.sum(u ** 2))
+
+    r += dt * u / gamma
+    return r, u, gamma
+
+def simulate_particle(particle_params):
+    charge, mass, r0, u0, dt, Nt = particle_params
+
+    part = Particle(mass, charge)
+    part.initPos(r0[0], r0[1], r0[2])
+    part.initSpeed(u0[0], u0[1], u0[2])
+
+    # Prepare arrays to hold trajectory data
+    x = np.zeros(Nt)
+    y = np.zeros(Nt)
+    z = np.zeros(Nt)
+    ux = np.zeros(Nt)
+    uy = np.zeros(Nt)
+    uz = np.zeros(Nt)
+    gamma = np.zeros(Nt)
+
+    for i in range(Nt):
+        part.r, part.u, part.gamma = push(part.r, part.u, part.gamma, charge, mass, dt)
+        x[i], y[i], z[i] = part.r
+        ux[i], uy[i], uz[i] = part.u
+        gamma[i] = part.gamma
+
+    return x, y, z, ux, uy, uz, gamma
+
+def parallel_simulation_with_progress(particle_params_list, num_processors=4):
+    results = []
+    with Pool(processes=num_processors) as pool:
+        with tqdm(total=len(particle_params_list), desc='Simulating Particles') as pbar:
+            for params in particle_params_list:
+                result = pool.apply_async(simulate_particle, args=(params,), callback=lambda _: pbar.update())
+                results.append(result)
+            pool.close()
+            pool.join()
+    return [res.get() for res in results]
+
+if __name__ == '__main__':
+    set_start_method('fork')
+
+    N = 1  # Number of particles
+    charge, mass = 1.0, 1.0
+
+    # Random initial positions and speeds for particles
+    r0_list = np.random.uniform([-1, -1, 0], [1, 1, 0], size=(N, 3))
+    u0_list = np.random.uniform([-0.1, -0.1, -0.1], [0.1, 0.1, 0.1], size=(N, 3))
+
+    # Calculate initial parameters
+    gamma0 = np.sqrt(1 + np.sum(u0_list[0] ** 2))
+    w0 = np.abs(charge) * np.sqrt(np.sum(B(r0_list[0]) * B(r0_list[0]))) / (mass / np.min(gamma0))
+    dt = 0.0001 / w0
+    Np = 50.0  # Number of periods for longest period particle at t=0
+    Tf = Np * 2 * np.pi / w0
+    Nt = int(Tf // dt)
+
+    num_processors = 10
+    particle_params_list = [(charge, mass, r0_list[i], u0_list[i], dt, Nt) for i in range(N)]
+
+    # Run simulations in parallel
+    results = parallel_simulation_with_progress(particle_params_list, num_processors)
+
+    # Save data to an HDF5 file
+    with h5py.File('particle_data.h5', 'w') as f:
+        f.attrs['w0'] = w0  # Save w0 as an attribute
+        f.attrs['dt'] = dt  # Save dt as an attribute
+        for index, traj in enumerate(results):
+            x, y, z, ux, uy, uz, gamma = traj
+            grp = f.create_group(f'particle_{index}')
+            grp.create_dataset('x', data=x)
+            grp.create_dataset('y', data=y)
+            grp.create_dataset('z', data=z)
+            grp.create_dataset('ux', data=ux)
+            grp.create_dataset('uy', data=uy)
+            grp.create_dataset('uz', data=uz)
+            grp.create_dataset('gamma', data=gamma)
+
+
---
+++ plotting_Boris copy.py
@@ -0,0 +1,66 @@
+#%%
+import numpy as np
+import h5py
+import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
+from matplotlib import cm
+
+def plot_particle_trajectories(file_name):
+    with h5py.File(file_name, 'r') as f:
+        # Retrieve w0 and dt from attributes
+        w0 = f.attrs['w0']
+        dt = f.attrs['dt']
+        
+        
+        N = len(f.keys())  # Number of particles
+        colors = cm.viridis(np.linspace(0, 1, N))
+
+        # Plotting all particle trajectories in 3D
+        fig = plt.figure()
+        ax = fig.add_subplot(111, projection='3d')
+
+        for index in range(N):
+            grp = f[f'particle_{index}']
+            x = grp['x'][:]
+            y = grp['y'][:]
+            z = grp['z'][:]
+            ax.plot(x, y, z, color=colors[index])
+
+        ax.set_xlabel('$x$', labelpad=15)
+        ax.set_ylabel('$y$', labelpad=15)
+        ax.set_zlabel('$z$', labelpad=2.5)
+        plt.show()
+        plt.close(fig)
+
+        # Efficient plotting of velocities and gamma
+        fig, axs = plt.subplots(4, 1, figsize=(8, 12))
+        
+        for index in range(N):
+            grp = f[f'particle_{index}']
+            ux = grp['ux'][:]
+            uy = grp['uy'][:]
+            uz = grp['uz'][:]
+            gamma = grp['gamma'][:]
+            Nt = len(ux)  # Number of time steps
+            t = np.arange(Nt) * dt  # Create the time array using dt
+
+            axs[0].plot(t * w0 / (2 * np.pi), ux/gamma, color=colors[index])
+            axs[1].plot(t * w0 / (2 * np.pi), uy/gamma, color=colors[index])
+            axs[2].plot(t * w0 / (2 * np.pi), uz/gamma, color=colors[index])
+            axs[3].plot(t * w0 / (2 * np.pi), gamma, color=colors[index])
+
+        axs[0].set_ylabel('$v_x$')
+        axs[1].set_ylabel('$v_y$')
+        axs[2].set_ylabel('$v_z$')
+        axs[3].set_ylabel('$\\gamma$')
+        axs[3].set_xlabel('$t \\omega_0 / 2\\pi$')
+        plt.show()
+        plt.close(fig)
+
+#%%
+file_name = 'particle_data.h5'
+
+pathto = '/Users/michaelgrehan/Desktop/Boris_MPG/'
+plot_particle_trajectories(pathto+file_name)
+
+# %%
